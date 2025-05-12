360fa2c4bd708e55fb7337b845de6cbcafa892f3
Neil Chan <neil.chan@card.rakuten.com.tw>
2025-05-12 10:24:23 +0800

    Initial Commit

---
+++ TCA_Login_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Login_Practice
+//
+//  Created by Neil Chan on 2025/5/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Login_Practice/TCA_Login_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Login_PracticeApp.swift
+//  TCA_Login_Practice
+//
+//  Created by Neil Chan on 2025/5/12.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Login_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
